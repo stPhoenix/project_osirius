7a485cce3940c88d9e20f21b57717478e996cef9
stPhoenix <bohdansapovsky@gmail.com>
2018-02-13 22:25:54 +0200

    Added more tests and some changes in core rev2

--- linguist/core.py
+++ linguist/core.py
@@ -1,4 +1,4 @@
-from linguist.models import Word, GlobalWord, Category
+from linguist.models import Word, GlobalWord, Language, Category
 from users.models import Student
 from random import randint
 
@@ -36,60 +36,42 @@ class LinguistHQ:
             return {'global_word_search': global_word_search, 'google_translate': google_translate,
                     'words': words}
 
-    def add_custom_word(self, word_name=None, translation=None, category_id=None):
-        if word_name is None or translation is None or category_id is None:
-            raise ValueError("Word can not be None")
-        category = Category.objects.get(pk=category_id)
+    def add_custom_word(self, word_name=None, translation=None, category=None):
+        error = None
+        if word_name is None or translation is None:
+            error = 'You did not choose word or translation or category'
+            return error
+        if category is None:
+            category = Category.objects.get(name="Default")
         word = Word(
             name=word_name,
             translation=translation,
-            language=self.student.current_language,
-            student=self.student,
-            category=category
+            language=Language.objeects.get(name=self.student.current_language),
+            student=self.student
         )
+        word.save()
+        word.category_set.add(category)
         word.save()
 
     def get_all_words(self):
-        return self.student.word__set().all()
+        return self.student.word_set.all()
 
     def get_viewed_words(self):
-        return self.student.word__set().filter(viewed=False, current_language=self.student.current_language)
+        return self.student.word_set.filter(viewed=True, language=Language.objects.get(name=self.student.current_language))
 
-    def set_viewed_word_status(self, word_id=None, status=False):
-        if word_id is None:
-            raise ValueError("Word id can not be None")
-        word = Word.objects.get(pk=int(word_id))
-        word.viewed = status
-        word.save()
+    def get_not_viewed_words(self):
+        return self.student.word_set.filter(viewed=False, language=Language.objects.get(name=self.student.current_language))
 
-    def play_matching(self):
-        words = self.student.word__set().all()
+    def play_matching(self, reverse=False):
+        words = self.student.word_set.filter(language=Language.objects.get(name=self.student.current_language))
+        words = words.filter(played_match=False) if reverse is False else words.filter(played_reversed_match=True)
         word = words[randint(1, words.count())]
         fake_words = map(words[randint(1, words.count())] in range(0, 3))
         fake_words.append(word.name)
         return {'words': fake_words, 'answer': word}
 
-    def set_played_matching_word_status(self, word_id=None, status=False, reverse=False):
-        if word_id is None:
-            raise ValueError("Word id can not be None")
-        word = Word.objects.get(pk=int(word_id))
-        if reverse is False:
-            word.played_match = status
-        else:
-            word.played_reversed_match = status
-        word.save()
-
-    def play_typing(self):
-        words = self.student.word__set().all()
+    def play_typing(self, reverse=False):
+        words = self.student.word_set.filter(language=Language.objects.get(name=self.student.current_language))
+        words = words.filter(played_typing=False) if reverse is False else words.filter(played_reversed_typing=True)
         word = words[randint(1, words.count())]
         return word
-
-    def set_played_typing_word_status(self, word_id=None, status=False, reverse=False):
-        if word_id is None:
-            raise ValueError("Word id can not be None")
-        word = Word.objects.get(pk=int(word_id))
-        if reverse is False:
-            word.played_typing = status
-        else:
-            word.played_reversed_typing = status
-        word.save()
--- linguist/tests.py
+++ linguist/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from users.models import Student
-from linguist.models import Language, GlobalWord, Word
+from linguist.models import Language, GlobalWord, Word, Category
 from linguist.core import LinguistHQ
 # Create your tests here.
 
@@ -16,10 +16,12 @@ class TestLinguistHQ(TestCase):
         self.home_language.save()
         self.current_language = Language.objects.create(slug='ja', name='Japanese')
         self.current_language.save()
-        self.user.language_set().add(self.current_language)
+        self.user.language_set.add(self.current_language)
         self.user.save()
         self.global_word = GlobalWord.objects.create(name='ありがとう', translation='thank you', language=self.current_language)
         self.global_word.save()
+        self.category = Category.objects.create(name='Default')
+        self.category.save()
         words = [
             ('おはよう', 'hello'),
             ('すみません', 'excuse me'),
@@ -35,10 +37,16 @@ class TestLinguistHQ(TestCase):
                 student=self.user
             )
             w.save()
+            w.category_set.add(self.category)
+            w.save()
+        for word in self.user.word_set.all()[:2]:
+            word.viewed = True
+            word.save()
 
     def test_init(self):
         l_hq = LinguistHQ(student_id=self.user.pk)
         self.assertIsInstance(l_hq.student, Student)
+        # Let's test non existing student check
         with self.assertRaises(ValueError):
             l_hq = LinguistHQ(student_id=9124124)
 
@@ -50,10 +58,54 @@ class TestLinguistHQ(TestCase):
         word[0].delete()
         # Let's test alternative arg
         l_hq.add_from_global_word(word_id=self.global_word.pk, alternative_translation='Alternative')
-        word2=self.user.word_set.filter(name=self.global_word.name, language=self.current_language)
-        self.assertEqual(self.global_word.translation, word[0].translation)
+        word2 = self.user.word_set.filter(name=self.global_word.name, language=self.current_language)
+        self.assertEqual(self.global_word.translation, word2[0].translation)
+        # Let's check if returning error
+        error = l_hq.add_from_global_word()
+        self.assertEqual(error, 'Please choose word')
 
     def test_search_word(self):
         l_hq = LinguistHQ(student_id=self.user.pk)
         words = l_hq.search_word('ありがとう', self.current_language)
         self.assertEqual(words['words'][0].translation, 'thank you')
+
+    def test_add_custom_word(self):
+        l_hq = LinguistHQ(student_id=self.user.pk)
+        l_hq.add_custom_word( word_name='あいさつ', translation='Greetings')
+        word = self.user.word_set.filter(name='あいさつ', language=Language.objects.get(name=self.user.current_language))
+        self.assertEqual(word[0].name, 'あいさつ')
+        # Let's test error
+        error = l_hq.add_custom_word()
+        self.assertEqual(error, 'You did not choose word or translation or category')
+
+    def test_get_all_words(self):
+        l_hq = LinguistHQ(student_id=self.user.pk)
+        self.assertEqual(l_hq.get_all_words(), self.user.word_set.all())
+
+    def test_get_viewed_words(self):
+        l_hq = LinguistHQ(student_id=self.user.pk)
+        words = self.user.word_set.filter(viewed=True,
+                                          language=Language.objects.get(name=self.user.current_language))
+        self.assertEqual(l_hq.get_viewed_words(), words)
+
+    def test_get_not_viewed_words(self):
+        l_hq = LinguistHQ(student_id=self.user.pk)
+        words = self.user.word_set.filter(viewed=False,
+                                          language=Language.objects.get(name=self.user.current_language))
+        self.assertEqual(l_hq.get_viewed_words(), words)
+
+    def test_play_matching(self):
+        l_hq = LinguistHQ(student_id=self.user.pk)
+        words = l_hq.play_matching()
+        self.assertIn(words['answer'], self.user.word_set.all())
+        # Let's test reverse case
+        words = l_hq.play_matching(reverse=True)
+        self.assertIn(words['answer'], self.user.word_set.all())
+
+    def test_play_typing(self):
+        l_hq = LinguistHQ(student_id=self.user.pk)
+        word = l_hq.play_typing()
+        self.assertIn(word, self.user.word_set.all())
+        # Let's test reverse case
+        word = l_hq.play_typing(reverse=True)
+        self.assertIn(word, self.user.word_set.all())
