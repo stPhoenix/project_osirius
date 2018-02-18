a2071330c54dc40b508c2ff7511136c894129b75
stPhoenix <bohdansapovsky@gmail.com>
2018-02-18 14:45:46 +0200

    Linguist Core refactor

--- linguist/core.py
+++ linguist/core.py
@@ -7,6 +7,7 @@ class LinguistHQ:
     def __init__(self, student=None):
         self.student = student
         self.langs = Language.objects.all()
+        self.global_words = GlobalWord.objects.all()
         if self.student is None:
             raise ValueError("Student can't be None")
 
@@ -15,18 +16,18 @@ class LinguistHQ:
         if word_id is None:
             error = 'Please choose word'
             return error
-        global_word = GlobalWord.objects.get(pk=word_id)
+        global_word = self.global_words.get(pk=word_id)
         word = Word(
             name=global_word.name,
             translation=alternative_translation if alternative_translation is not None else global_word.translation,
-            language=Language.objects.get(name=self.student.current_language),
+            language=self.langs.get(name=self.student.current_language),
             student=self.student
         )
         word.save()
         word.category_set.set(global_word.category_set.all())
 
     def search_word(self, word_name=None, language=None):
-        words = GlobalWord.objects.filter(name=word_name, language=language)
+        words = self.global_words.filter(name=word_name, language=language)
         global_word_search = False
         google_translate_search = False
         home_language = self.langs.get(name=self.student.home_language)
@@ -50,7 +51,7 @@ class LinguistHQ:
         word = Word(
             name=word_name,
             translation=translation,
-            language=Language.objects.get(name=self.student.current_language),
+            language=self.langs.get(name=self.student.current_language),
             student=self.student
         )
         word.save()
@@ -61,13 +62,13 @@ class LinguistHQ:
         return self.student.word_set.all()
 
     def get_viewed_words(self):
-        return self.student.word_set.filter(viewed=True, language=Language.objects.get(name=self.student.current_language))
+        return self.student.word_set.filter(viewed=True, language=self.get(name=self.student.current_language))
 
     def get_not_viewed_words(self):
-        return self.student.word_set.filter(viewed=False, language=Language.objects.get(name=self.student.current_language))
+        return self.student.word_set.filter(viewed=False, language=self.get(name=self.student.current_language))
 
     def play_matching(self, reverse=False):
-        words = self.student.word_set.filter(language=Language.objects.get(name=self.student.current_language))
+        words = self.student.word_set.filter(language=self.get(name=self.student.current_language))
         words = words.filter(played_match=False) if reverse is False else words.filter(played_reversed_match=False)
         count = words.count()
         if count == 0:
@@ -79,7 +80,7 @@ class LinguistHQ:
             return {'words': fake_words, 'answer': word}
 
     def play_typing(self, reverse=False):
-        words = self.student.word_set.filter(language=Language.objects.get(name=self.student.current_language))
+        words = self.student.word_set.filter(language=self.langs.get(name=self.student.current_language))
         words = words.filter(played_typing=False) if reverse is False else words.filter(played_reversed_typing=False)
         count = words.count()
         if count == 0:
