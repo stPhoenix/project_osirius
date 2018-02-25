5b13adffc2003b61a825184b9b7eed7a3a023364
stPhoenix <bohdansapovsky@gmail.com>
2018-02-25 12:06:11 +0200

    Updated lean words structure

--- telegram_bot/modulus/learnwords.py
+++ telegram_bot/modulus/learnwords.py
@@ -17,12 +17,40 @@ class LearnWords(BaseModule):
 
     def setup_destinations(self):
         self.DESTINATIONS = {
-            'Learn words': 'self.learn_words',
+            'Learn words': self.learn_words,
+            'Learn word status': self.learn_word_status,
             'Play matching': 'self.play_matching',
             'Play reversed matching': 'self.play_reversed_matching',
             'Play typing': 'self.play_typing',
             'Play reversed typing': 'self.play_reversed_typing',
         }
 
-    def learn_words(self):
+    def learn_words(self, bot, update, student):
+        pass
+
+    def learn_word_status(self, bot, update, student):
+        pass
+
+    def play_matching(self, bot, update, student):
+        pass
+
+    def play_matching_result(self, bot, update, student):
+        pass
+
+    def play_reversed_matching(self, bot, update, student):
+        pass
+
+    def play_reversed_matching_result(self, bot, update, student):
+        pass
+
+    def play_typing(self, bot, update, student):
+        pass
+
+    def play_typing_result(self, bot, update, student):
+        pass
+
+    def play_reversed_typing(self, bot, update, student):
+        pass
+
+    def play_reversed_typing_result(self, bot, update, student):
         pass
