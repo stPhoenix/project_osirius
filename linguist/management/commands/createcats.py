987f6b8e398ec285b41e4889027b362278b8ea15
stPhoenix <bohdansapovsky@gmail.com>
2018-03-06 22:37:14 +0200

    Modified create cats

--- linguist/management/commands/createcats.py
+++ linguist/management/commands/createcats.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from linguist.models import Category
+from django.db.models import ObjectDoesNotExist
 
 
 class Command(BaseCommand):
@@ -10,6 +11,16 @@ class Command(BaseCommand):
         'Greetings',
         'New friends',
         'Countries',
+        'Majors',
+        'Occupations',
+        'Family',
+        'Numbers',
+        'Words that point',
+        'Food',
+        'Things',
+        'Places',
+        'Money matter',
+        'Expressions',
             ]
 
     def add_arguments(self, parser):
@@ -17,8 +28,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for c in self.cats:
-            cat = Category.objects.filter(name=c)
-            if cat is not None:
+            try:
+                Category.objects.get(name=c)
+            except ObjectDoesNotExist:
                 category = Category(name=c)
                 category.save()
                 print('Added %s' % c)
