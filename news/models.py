aa080ac9c76d9bac27d59e383a8d4a1a4f1a0f48
stPhoenix <bohdansapovsky@gmail.com>
2018-05-06 21:57:47 +0300

    Added article model for news

--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
