6460de0745e05276b1cfa8f2a3039bd543d59403
ST <bohdansapovsky@gmail.com>
2018-05-10 19:01:16 +0300

    Updated frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/AppComponent';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppComponent />, document.getElementById('root'));
 registerServiceWorker();
