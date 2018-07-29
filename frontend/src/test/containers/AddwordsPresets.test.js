5b2274142e9cedaf16e9102007f8a2100c5eca4f
StPhoenix <bohdansapovsky@gmail.com>
2018-07-29 22:42:56 +0300

    Added redux mock store

--- frontend/src/test/containers/AddwordsPresets.test.js
+++ frontend/src/test/containers/AddwordsPresets.test.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import AddwordsPresets from '../../containers/AddwordsPresets';
 import {shallow} from 'enzyme';
-import {connect} from 'react-redux';
+import configureStore from 'redux-mock-store';
+import {add_global_word, get_cats, get_words_by_cat} from '../../api/0';
 
-jest.mock(connect);
+jest.mock('../../api/0');
 
 describe('AddwordsPresets Container', () => {
     const classProps = {
-                        dispatch: jest.fn(),
-                        token: "xxx",
-                        isAuthenticated: true,
+                        auth: {token: "xxx", isAuthenticated: true},
                         };
-    const container = shallow(<AddwordsPresets {...classProps} />);
-    it('Should get categories', () => {
-        container.props.componentDidMount();
-        expect(dispatch).toHaveBeenCalled();
+    const mockStore = configureStore();
+    let store =  mockStore(classProps);
+    get_cats.mockResolvedValue({result: true, data: [{name: "Default", pk: "1"}]});
+    const words = [{name: "ありがとう",
+                    translation: "Thank you",
+                    pronunciation: "Arigato",
+                    category_set: [{name: "Default", pk: "1"}],
+                    pk: "1"}];
+    get_words_by_cat.mockResolvedValue({result: true, data: words});
+    
+    const container = shallow(<AddwordsPresets store={store} />);
+    
+    it('Should get categories and words by cats', () => {
+        container.instance();
+        container.update();
+        console.log(container.instance().handleChange);
+        expect(container.state().words).toEqual(words);
     });
 });
